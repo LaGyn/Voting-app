b7061c6a255eee29e586100d0471b6983514c047
Laura Gynther <lauragynther@live.com>
2022-08-16 16:27:00 +0300

    lisättiin localstrorage

--- main.js
+++ main.js
@@ -18,7 +18,15 @@ function User(firstname, lastname, address, zipcode, email, username, password){
     this.password = password;
 }
 
-function addUser(){
+let submitBtn = document.forms['signupform'];
+let submit = document.forms['loginform'];
+
+submitBtn.addEventListener('submit', addUser)
+submit.addEventListener('submit', logIn)
+
+function addUser(event){
+    //event.preventDefault()
+
     let firstname = document.getElementById("firstname").value;
     let lastname = document.getElementById("lastname").value;
     let address = document.getElementById("address").value;
@@ -30,32 +38,43 @@ function addUser(){
     let user;
     user = new User(firstname, lastname, address, zipcode, email, username, password);
     users.push(user);
+    localStorage.setItem('firstname', firstname);
+    localStorage.setItem('lastname', lastname);
+    localStorage.setItem('address', address);
+    localStorage.setItem('zipcode', zipcode);
+    localStorage.setItem('email', email);
+    localStorage.setItem('username', username);
+    localStorage.setItem('password', password);
 
-    document.getElementById("firstname").value = "";
+   /* document.getElementById("firstname").value = "";
     document.getElementById("lastname").value = "";
     document.getElementById("address").value = "";
     document.getElementById("zipcode").value = "";
     document.getElementById("email").value = "";
     document.getElementById("username").value = "";
-    document.getElementById("password").value = "";
+    document.getElementById("password").value = "";*/
 }
 
-let tulosta = "";
-function logIn(){
+
+function logIn(event){
+    event.preventDefault()
+    const name = localStorage.getItem('username');
+    const word = localStorage.getItem('password');
     let appliedName = document.getElementById("Username").value;
     let appliedPassword = document.getElementById("Password").value;
     let inList = false;
+    if (name == appliedName && word == appliedPassword){
+        window.open('/admin.html');
+    }
     for (let i = 0; i < users.length; i++){
-        if (users[i].username == appliedName && users[i].password == appliedPassword){
+        if (appliedName == 'yllapito' && appliedPassword == 'lintu'){
             inList = true;
-            tulosta = "true";
+            window.open('/admin.html');
         }
-        else if (inList == false){
-            tulosta = "false!";
+        else if(users[i].username == appliedName && users[i].password == appliedPassword){
+            inList = true;
+            window.open('/home.html');
         }
-        alert(tulosta);
-        document.getElementById("Username").value = "";
-        document.getElementById("Password").value = "";
     }
     
 }
