f3231b14bbca23bbc994e44313530f99bc452214
Laura Gynther <lauragynther@live.com>
2022-08-27 11:18:16 +0300

    äänestysten selaaminen toimimaan

--- vote.js
+++ vote.js
@@ -1,6 +1,36 @@
-window.addEventListener('load', createListOfVotes);
-
+window.addEventListener('load', createListOfVotes); // Kun sivu latautuu suoritetaan seuraava funktio 
+//let votes = [];
+/*function Vote(VoteName, candidates){
+    this.VoteName = VoteName;
+    this.candidates = candidates;
+}*/
 function createListOfVotes(){
-    let data = JSON.parse(localStorage.getItem('votes'));
+    const data = JSON.parse(localStorage.getItem('votes'));
     console.log(data)
+    let VoteName;
+    for (let i = 0; i < data.length; i++){
+        VoteName = data[i].VoteName; // KOrjaa tämä!!!
+        //votes.push(data[i].VoteName);
+        let votename = VoteName;
+        let voteElement = document.createElement('a');
+        let voteTitle = document.createTextNode(votename);
+        voteElement.appendChild(voteTitle);
+        voteElement.className = 'vote-item';
+        voteElement.href = "#"; // Tähän linkkiosoite
+        let linebreak = document.createElement('br');
+        document.querySelector('#printArea3').appendChild(voteElement);
+        document.querySelector('#printArea3').appendChild(linebreak);
+    }
+   /* data.forEach(vote => {
+        let votename = VoteName;
+        let voteElement = document.createElement('a');
+        let voteTitle = document.createTextNode(votename);
+        voteElement.appendChild(voteTitle);
+        voteElement.className = 'vote-item';
+        voteElement.href = "#"; // Tähän linkkiosoite
+        let linebreak = document.createElement('br');
+        document.querySelector('#printArea3').appendChild(voteElement);
+        document.querySelector('#printArea3').appendChild(linebreak);
+    })*/
+    //console.log(votes);
 }
